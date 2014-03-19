a34d0c97380edadac25c04bbbdf0d56888abd8f4
Uri Agassi <uri.agassi@perion.com>
2014-03-19 15:58:16 +0200

    allow extra instance tags

--- bin/userdata.sh
+++ bin/userdata.sh
@@ -3,9 +3,10 @@
 # usage: userdata_run.sh app_name instance_name deploy
 
 
+if [ -z ${INSTANCE_TAGS} ];then
+  INSTANCE_TAGS="{\"Name\":\"$2\",\"deploy\":\"$3\"}"
+fi
 
-INSTANCE_TAGS="{\"Name\":\"$2\",\"deploy\":\"$3\"}"
-
-/home/ubuntu/prep_instance.py -g $INSTANCE_TAGS -t app_$1
+/home/ubuntu/prep_instance.py -g ${INSTANCE_TAGS} -t app_$1
 
 ./create_mount.sh $1
